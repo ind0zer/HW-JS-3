a38bbc763c09bcb959c8e10457bb6d022b3018a8
ind0zer <165696434+ind0zer@users.noreply.github.com>
2024-05-27 18:56:34 +0300

    homework

---
+++ main.js
@@ -0,0 +1,19 @@
+const result = 5+5+`5`;
+console.log(typeof result);
+console.log(result);
+console.log(" ");
+
+const email = "artemznoyak@gmail.com"
+console.log(email.includes("@"))
+console.log(email.length)
+console.log(" ");
+
+const my = "my";
+const name = "name";
+const is = "is";
+const fullname = `${my} ${name} ${is} Artem`;
+console.log(fullname);
+
+const username = "indozer";
+const payment = "300";
+alert(`Дякуємо, ${username}! До сплати ${payment} гривень.`);
